345902a6bdc5908a60fb01921a88f8065ee6be16
hussein alamutu <alamutuhussein@gmail.com>
2022-05-27 00:05:51 +0100

    edit

--- catalog/views.py
+++ catalog/views.py
@@ -1,54 +1,73 @@
 from django.shortcuts import render
-from .models import Book, Author, BookInstance, Genre
-from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.mixins import PermissionRequiredMixin
 
-# Create your views here
+# Create your views here.
+
+from .models import Book, Author, BookInstance, Genre
+
 
 def index(request):
-    """View function for homepage of site"""
-
+    """View function for home page of site."""
     # Generate counts of some of the main objects
-
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
-
-    # Available Books
+    # Available copies of books
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
-    num_genre_word = Book.objects.filter(genre__name__icontains='financial').count()
-    num_books_word = Book.objects.filter(title__icontains='rich').count()
+    num_authors = Author.objects.count()  # The 'all()' is implied by default.
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits+1
+
+    # Render the HTML template index.html with the data in the context variable.
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_visits': num_visits},
+    )
 
 
-    #The 'all()' is implied by default
-    num_authors = Author.objects.count()
-    num_visits = request.session.get('num_visits', 0)
-    request.session['num_visits'] = num_visits + 1
+from django.views import generic
 
-    context = {'num_books': num_books, 'num_instances': num_instances, 'num_instances_available': num_instances_available, 'num_authors': num_authors, 'num_genre_word': num_genre_word, 'num_books_word': num_books_word, 'num_visits': num_visits,}
-
-    return render(request, 'index.html', context=context)
 
 class BookListView(generic.ListView):
+    """Generic class-based view for a list of books."""
     model = Book
+    paginate_by = 10
 
 
 class BookDetailView(generic.DetailView):
+    """Generic class-based detail view for a book."""
     model = Book
 
+
 class AuthorListView(generic.ListView):
+    """Generic class-based list view for a list of authors."""
     model = Author
+    paginate_by = 10
+
 
 class AuthorDetailView(generic.DetailView):
+    """Generic class-based detail view for an author."""
     model = Author
 
-class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     """Generic class-based view listing books on loan to current user."""
     model = BookInstance
-    template_name ='catalog/bookinstance_list_borrowed_user.html'
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
 
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+# Added as part of challenge!
+from django.contrib.auth.mixins import PermissionRequiredMixin
 
 
 class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
@@ -56,18 +75,21 @@ class LoanedBooksAllListView(PermissionRequiredMixin, generic.ListView):
     model = BookInstance
     permission_required = 'catalog.can_mark_returned'
     template_name = 'catalog/bookinstance_list_borrowed_all.html'
+    paginate_by = 10
 
     def get_queryset(self):
         return BookInstance.objects.filter(status__exact='o').order_by('due_back')
 
-import datetime
 
-from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+import datetime
+from django.contrib.auth.decorators import login_required, permission_required
 
+# from .forms import RenewBookForm
 from catalog.forms import RenewBookForm
+
 
 @login_required
 @permission_required('catalog.can_mark_returned', raise_exception=True)
@@ -80,7 +102,7 @@ def renew_book_librarian(request, pk):
 
         # Create a form instance and populate it with data from the request (binding):
         form = RenewBookForm(request.POST)
-        
+
         # Check if the form is valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
@@ -88,9 +110,9 @@ def renew_book_librarian(request, pk):
             book_instance.save()
 
             # redirect to a new URL:
-            return HttpResponseRedirect(reverse('all-borrowed') )
+            return HttpResponseRedirect(reverse('all-borrowed'))
 
-    # If this is a GET (or any other method) create the default form.
+    # If this is a GET (or any other method) create the default form
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
         form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
@@ -102,20 +124,24 @@ def renew_book_librarian(request, pk):
 
     return render(request, 'catalog/book_renew_librarian.html', context)
 
+
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from .models import Author
 
-from catalog.models import Author
+
 class AuthorCreate(PermissionRequiredMixin, CreateView):
     model = Author
     fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
     initial = {'date_of_death': '11/06/2020'}
     permission_required = 'catalog.can_mark_returned'
 
+
 class AuthorUpdate(PermissionRequiredMixin, UpdateView):
     model = Author
     fields = '__all__' # Not recommended (potential security issue if more fields added)
     permission_required = 'catalog.can_mark_returned'
+
 
 class AuthorDelete(PermissionRequiredMixin, DeleteView):
     model = Author
@@ -123,19 +149,22 @@ class AuthorDelete(PermissionRequiredMixin, DeleteView):
     permission_required = 'catalog.can_mark_returned'
 
 
-
+# Classes created for the forms challenge
 class BookCreate(PermissionRequiredMixin, CreateView):
     model = Book
     fields = ['title', 'author', 'summary', 'isbn', 'genre', 'language']
     permission_required = 'catalog.can_mark_returned'
-    
+
+
 class BookUpdate(PermissionRequiredMixin, UpdateView):
     model = Book
     fields = ['title', 'author', 'summary', 'isbn', 'genre', 'language']
     permission_required = 'catalog.can_mark_returned'
 
+
 class BookDelete(PermissionRequiredMixin, DeleteView):
     model = Book
     success_url = reverse_lazy('books')
     permission_required = 'catalog.can_mark_returned'
+
 
