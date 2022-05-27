1dbd0a70c9a1449f943a98de823ce72a711276d8
hussein alamutu <alamutuhussein@gmail.com>
2022-05-27 11:59:40 +0100

    uuid

--- catalog/models.py
+++ catalog/models.py
@@ -69,7 +69,7 @@ from django.contrib.auth.models import User  # Required to assign User as a borr
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+    id = models.UUIDField(primary_key=True, default=uuid.uuid1,
                           help_text="Unique ID for this particular book across whole library")
     book = models.ForeignKey('Book', on_delete=models.RESTRICT, null=True)
     imprint = models.CharField(max_length=200)
